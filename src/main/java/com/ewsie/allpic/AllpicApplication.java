1a77206201f73b869edc1c4e5dd9dca7f25b8771
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2020-04-02 21:53:42 +0200

    Spring Initializr project structure created

---
+++ src/main/java/com/ewsie/allpic/AllpicApplication.java
@@ -0,0 +1,13 @@
+package com.ewsie.allpic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllpicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllpicApplication.class, args);
+    }
+
+}
