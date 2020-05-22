880b792a987377972032e183c5b49d721d7534e5
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2020-05-23 01:12:14 +0200

    [Feature/7] Added endpoint for downloading images by token

--- allpic-backend/src/main/java/com/ewsie/allpic/image/controller/impl/ImageControllerImpl.java
+++ allpic-backend/src/main/java/com/ewsie/allpic/image/controller/impl/ImageControllerImpl.java
@@ -3,13 +3,18 @@ package com.ewsie.allpic.image.controller.impl;
 import com.amazonaws.SdkClientException;
 import com.ewsie.allpic.image.controller.ImageController;
 import com.ewsie.allpic.image.model.ImageDTO;
+import com.ewsie.allpic.image.model.ImageDTOWithContent;
 import com.ewsie.allpic.image.model.ImageDetails;
+import com.ewsie.allpic.image.service.LoadImageService;
 import com.ewsie.allpic.image.service.SaveImageService;
 import com.ewsie.allpic.user.model.CustomUserDetails;
 import com.ewsie.allpic.user.model.UserDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,11 +26,25 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ImageControllerImpl implements ImageController {
 
+    private final LoadImageService loadImageService;
     private final SaveImageService saveImageService;
 
     @Override
-    public ResponseEntity<String> getImage(String id) {
-        return null;
+    public ResponseEntity<Resource> getImage(String token) {
+        ImageDTOWithContent requestedImage;
+
+        try {
+            requestedImage = loadImageService.loadImage(token);
+        } catch (NullPointerException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (SdkClientException e) {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+        }
+
+        return ResponseEntity.ok()
+                .contentLength(requestedImage.getContentLength())
+                .contentType(MediaType.parseMediaType(requestedImage.getContentType()))
+                .body(new InputStreamResource(requestedImage.getContent()));
     }
 
     @Override
---
+++ allpic-backend/src/main/java/com/ewsie/allpic/image/service/impl/LoadImageServiceImpl.java
@@ -0,0 +1,57 @@
+package com.ewsie.allpic.image.service.impl;
+
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.ewsie.allpic.config.AppConfig;
+import com.ewsie.allpic.image.model.ImageDTO;
+import com.ewsie.allpic.image.model.ImageDTOWithContent;
+import com.ewsie.allpic.image.service.ImageDTOService;
+import com.ewsie.allpic.image.service.LoadImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoadImageServiceImpl implements LoadImageService {
+
+    private final AppConfig appConfig;
+    private final ImageDTOService imageDTOService;
+
+    @Override
+    public ImageDTOWithContent loadImage(String token) throws NullPointerException, SdkClientException {
+        ImageDTO requestedImageDto = getImageDetails(token);
+
+        S3Object fullObject = getImageObjectFromS3(token);
+
+        ImageDTOWithContent res = ImageDTOWithContent.builder()
+                .imageDTO(requestedImageDto)
+                .content(fullObject.getObjectContent())
+                .contentType(fullObject.getObjectMetadata().getContentType())
+                .contentLength(fullObject.getObjectMetadata().getContentLength())
+                .build();
+
+        return res;
+    }
+
+    private ImageDTO getImageDetails(String token) {
+        Optional<ImageDTO> requestedImageDto = Optional.ofNullable(imageDTOService.findByToken(token));
+        if (requestedImageDto.isEmpty()) {
+            throw new NullPointerException("Content identified by " + token + " not found in database");
+        }
+
+        return requestedImageDto.get();
+    }
+
+    private S3Object getImageObjectFromS3(String token) {
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(appConfig.getAwsRegion())
+                .build();
+
+        return s3Client.getObject(new GetObjectRequest(appConfig.getS3StorageBucket(), token));
+    }
+}
