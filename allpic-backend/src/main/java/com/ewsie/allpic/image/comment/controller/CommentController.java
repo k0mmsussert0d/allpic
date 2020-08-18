77c16d54b10916f7826b7f3329257a11c06d2de8
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2020-08-18 23:19:47 +0200

    refactor() Documented comment endpoint (#1)

--- allpic-backend/src/main/java/com/ewsie/allpic/image/comment/controller/CommentController.java
+++ allpic-backend/src/main/java/com/ewsie/allpic/image/comment/controller/CommentController.java
@@ -2,6 +2,8 @@ package com.ewsie.allpic.image.comment.controller;
 
 import com.ewsie.allpic.image.comment.model.CommentDTO;
 import com.ewsie.allpic.user.model.CustomUserDetails;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,10 +12,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+@Api(tags={"Comments"})
 @RequestMapping("/comment")
 public interface CommentController {
 
-    @PutMapping("/{imageToken}")
+    @ApiOperation(value = "Gets all comments of the image")
+    @GetMapping("/{imageToken}")
+    ResponseEntity<List<CommentDTO>> getCommentsForImage(@PathVariable String imageToken);
+
+    @ApiOperation(value = "Posts a comment of the image")
+    @PostMapping("/{imageToken}")
     @PreAuthorize("isFullyAuthenticated()")
     ResponseEntity<String> addComment(
             @PathVariable String imageToken,
@@ -21,9 +29,7 @@ public interface CommentController {
             @RequestBody String message
     );
 
-    @GetMapping("{imageToken}")
-    ResponseEntity<List<CommentDTO>> getCommentsForImage(@PathVariable String imageToken);
-
+    @ApiOperation(value = "Deletes the comment")
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAnyRole('ROLE_MOD','ROLE_ADMIN')")
     ResponseEntity<Void> removeComment(@PathVariable Long id);
