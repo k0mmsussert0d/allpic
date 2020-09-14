1d8271d2d51f022e1dedc470a87393c349b34268
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2020-09-14 23:36:41 +0200

    tests() Added more tests for User controller. Changed date format for JSON serialization (#3)

--- allpic-backend/src/test/java/com/ewsie/allpic/user/controller/impl/UserControllerImplTest.java
+++ allpic-backend/src/test/java/com/ewsie/allpic/user/controller/impl/UserControllerImplTest.java
@@ -32,29 +32,53 @@ class UserControllerImplTest {
     @MockBean
     private ImageDTOService imageDTOService;
 
+    //language=JSON
+    private static final String DEFAULT_USERS_JSON =
+            "[" +
+            "{\"id\": null, \"username\": \"user1\", \"email\": \"user1@example.com\", \"registerTime\": \"2020-01-01T12:00:00\", \"isActive\": true, \"role\":  null}" +
+            "," +
+            "{\"id\": null, \"username\": \"user2\", \"email\": \"user2@example.com\", \"registerTime\": \"2020-01-01T12:00:00\", \"isActive\": true, \"role\":  null}" +
+            "," +
+            "{\"id\": null, \"username\": \"user3\", \"email\": \"user3@example.com\", \"registerTime\": \"2020-01-01T12:00:00\", \"isActive\": true, \"role\":  null}" +
+            "]";
+
+    @Test
+    public void whenGetUsers_asAnonymous_shouldReturnUnauthorized() throws Exception {
+
+        mockMvc.perform(get("/user/"))
+                .andExpect(status().isUnauthorized());
+    }
+
     @Test
     @WithMockUser(roles = {"ADMIN"})
-    public void whenUsers_shouldReturnUsers() throws Exception {
+    public void whenGetUsers_asAdmin_andUserListIsEmpty_shouldReturnNotFoundError() throws Exception {
 
-        UserDTO user1 = getSampleUser("user1");
-        UserDTO user2 = getSampleUser("user2");
-        UserDTO user3 = getSampleUser("user3");
-        List<UserDTO> users = List.of(user1, user2, user3);
+        Mockito.when(userDTOService.getAll()).thenReturn(null);
+
+        mockMvc.perform(get("/user/"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    public void whenGetUsers_asAdmin_andUserListIsNotEmpty_shouldReturnUsers() throws Exception {
+
+        List<UserDTO> users = getSampleUsers();
 
         Mockito.when(userDTOService.getAll()).thenReturn(users);
 
         mockMvc.perform(get("/user/"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(
-                        "[" +
-                                "{\"id\": null, \"username\": \"user1\", \"email\": \"user1@example.com\", \"registerTime\": \"2020-01-01 12:00\", \"isActive\": true, \"role\":  null}" +
-                                "," +
-                                "{\"id\": null, \"username\": \"user2\", \"email\": \"user2@example.com\", \"registerTime\": \"2020-01-01 12:00\", \"isActive\": true, \"role\":  null}" +
-                                "," +
-                                "{\"id\": null, \"username\": \"user3\", \"email\": \"user3@example.com\", \"registerTime\": \"2020-01-01 12:00\", \"isActive\": true, \"role\":  null}" +
-                                "]"
-                ));
+                .andExpect(content().json(DEFAULT_USERS_JSON));
+    }
+
+    private List<UserDTO> getSampleUsers() {
+        UserDTO user1 = getSampleUser("user1");
+        UserDTO user2 = getSampleUser("user2");
+        UserDTO user3 = getSampleUser("user3");
+
+        return List.of(user1, user2, user3);
     }
 
     private UserDTO getSampleUser(String username) {
