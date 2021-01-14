f7886f83237e02f7dcee9d835fcecd7bdf7d4ee1
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2021-01-14 22:32:42 +0100

    bugfix() Fixed a bug where app would not start on db initialization error (temporary)

--- allpic-backend/src/main/java/com/ewsie/allpic/config/DataLoader.java
+++ allpic-backend/src/main/java/com/ewsie/allpic/config/DataLoader.java
@@ -3,15 +3,20 @@ package com.ewsie.allpic.config;
 import com.ewsie.allpic.user.role.Role;
 import com.ewsie.allpic.user.role.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("!test")
 @RequiredArgsConstructor
 public class DataLoader implements ApplicationRunner {
+    
+    private final static Logger LOGGER = LoggerFactory.getLogger(DataLoader.class);
 
     private final RoleRepository roleRepository;
 
@@ -21,15 +26,19 @@ public class DataLoader implements ApplicationRunner {
     }
 
     private void initializeRoles() {
-        Role userRole = new Role();
-        userRole.setRoleName("USER");
-        Role modRole = new Role();
-        modRole.setRoleName("MOD");
-        Role adminRole = new Role();
-        adminRole.setRoleName("ADMIN");
+        try {
+            Role userRole = new Role();
+            userRole.setRoleName("USER");
+            Role modRole = new Role();
+            modRole.setRoleName("MOD");
+            Role adminRole = new Role();
+            adminRole.setRoleName("ADMIN");
 
-        roleRepository.save(userRole);
-        roleRepository.save(modRole);
-        roleRepository.save(adminRole);
+            roleRepository.save(userRole);
+            roleRepository.save(modRole);
+            roleRepository.save(adminRole);
+        } catch (DataIntegrityViolationException e) {
+            LOGGER.warn("Initialization sequence has not been properly executed");
+        }
     }
 }
